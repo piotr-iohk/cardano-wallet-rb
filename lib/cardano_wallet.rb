47da13e2e9d26820c34d4cf9e1cd42ea4ad60f6f
Piotr Stachyra <piotr.stachyra@iohk.io>
2020-09-28 12:14:57 +0200

    Move proxy to misc

--- lib/cardano_wallet.rb
+++ lib/cardano_wallet.rb
@@ -6,7 +6,6 @@ require_relative "cardano_wallet/utils"
 require_relative "cardano_wallet/shelley"
 require_relative "cardano_wallet/byron"
 require_relative "cardano_wallet/misc"
-require_relative "cardano_wallet/proxy"
 
 module CardanoWallet
   def self.new(options = {})
--- lib/cardano_wallet/misc.rb
+++ lib/cardano_wallet/misc.rb
@@ -20,6 +20,11 @@ module CardanoWallet
       def utils
         Utils.new @opt
       end
+
+      # @see https://input-output-hk.github.io/cardano-wallet/api/edge/#tag/Proxy
+      def proxy
+        Proxy.new @opt
+      end
     end
 
     # API for Network
@@ -60,6 +65,22 @@ module CardanoWallet
         self.class.get("/addresses/#{address_id}")
       end
 
+    end
+
+    # @see https://input-output-hk.github.io/cardano-wallet/api/edge/#tag/Proxy
+    class Proxy < Base
+      def initialize opt
+        super
+      end
+
+      # Submit a transaction that was created and signed outside of cardano-wallet.
+      # @see https://input-output-hk.github.io/cardano-wallet/api/edge/#operation/postExternalTransaction
+      # @param binary_blob [String] Signed transaction message binary blob.
+      def submit_external_transaction(binary_blob)
+        self.class.post("/proxy/transactions",
+                        :body => binary_blob,
+                        :headers => { 'Content-Type' => 'application/octet-stream' })
+      end
     end
   end
 end
